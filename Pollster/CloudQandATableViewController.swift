d1d0f2df471a08bf93c12d7ec813dd0491988e73
Ivan <ivan@MacBook-Air-Ivan.local>
2016-11-02 23:56:10 +0300

    Initial Commit

---
+++ Pollster/CloudQandATableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CloudQandATableViewController.swift
+//  Pollster
+//
+//  Created by Ivan on 02.11.16.
+//  Copyright © 2016 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class CloudQandATableViewController: QandATableViewController {
+
+
+
+}
